655ae0e8b26bfaa2a429181e249b80b23e14305c
samueldcabral <samueldcabral@gmail.com>
2019-01-19 00:12:54 -0300

    Updates

---
+++ precache-manifest.b72c53f729fbc3d09f5f60d11a4bc067.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d69eaf881fce1198fd7",
+    "url": "/Shopify-Coding_Challenge/static/js/runtime~main.0d69eaf8.js"
+  },
+  {
+    "revision": "fd9e2bda3e37d4462833",
+    "url": "/Shopify-Coding_Challenge/static/js/main.fd9e2bda.chunk.js"
+  },
+  {
+    "revision": "65e5ab301d1fdcc454a6",
+    "url": "/Shopify-Coding_Challenge/static/js/1.65e5ab30.chunk.js"
+  },
+  {
+    "revision": "fd9e2bda3e37d4462833",
+    "url": "/Shopify-Coding_Challenge/static/css/main.ff998455.chunk.css"
+  },
+  {
+    "revision": "4550bb506a06ea00738358ffb38d6deb",
+    "url": "/Shopify-Coding_Challenge/index.html"
+  }
+];
